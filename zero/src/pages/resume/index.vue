cedc59e9e9d1064758d85b69a015417fb8e4591d
lihanxi <3138220743@qq.com>
2022-08-03 19:17:43 +0800

    220803

--- zero/src/pages/resume/index.vue
+++ zero/src/pages/resume/index.vue
@@ -2,25 +2,23 @@
   <div class="resume">
     <div class="bread">
       <el-breadcrumb separator="/">
-        <el-breadcrumb-item :to="{ path: '/introduce' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: '/introduce' }"
+          >首页</el-breadcrumb-item
+        >
         <el-breadcrumb-item>简历</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
-    
   </div>
 </template>
 
 <script>
 export default {
-name:'resume',
-data() {
-  return {
-    
-  }
-},
-}
+  name: "resume",
+  data() {
+    return {};
+  },
+};
 </script>
 
 <style>
-
 </style>
