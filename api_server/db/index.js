7b7c9b0736c155db1be6b926ecdbe9c2995afed7
lihanxi <3138220743@qq.com>
2022-08-22 18:38:38 +0800

    '20200810'

--- api_server/db/index.js
+++ api_server/db/index.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 
 const db = mysql.createPool({
   host: '127.0.0.1',
-  port:'  3306',
+  port:'3306',
   user: 'root',
   password: 'admin123',
   database: 'myblog',
